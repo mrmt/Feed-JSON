7fbf526e5ce1743ca0679631cb695cef1783c9d9
Jun Morimoto <morimoto@mrmt.net>
2011-12-26 10:44:10 +0900

    alpha hack

--- feed-json-template.php
+++ feed-json-template.php
@@ -6,15 +6,19 @@
 $callback = trim(esc_html(get_query_var('callback')));
 $charset = get_bloginfo('charset');
 
-if (!function_exists('json_encode')) {
-	// For PHP < 5.2.0
-	function json_encode( $string ) {
-		if ( !class_exists('Services_JSON') ) {
-			require_once( 'class-json.php' );
-		}
-		$json = new Services_JSON();
-		return $json->encode( $string );
+function get_the_excerpt_max_charlength($charlength) {
+	$excerpt = get_the_excerpt();
+	if(strlen($excerpt) > $charlength) {
+		return mb_substr($excerpt, 0, $charlength);
 	}
+	return $excerpt;
+}
+
+// use our json_encode
+function my_json_encode( $string ) {
+	require_once( 'class-json.php' );
+	$json = new My_Services_JSON();
+	return $json->encode( $string );
 }
 
 if ( have_posts() ) {
@@ -24,37 +28,34 @@ if ( have_posts() ) {
 		$id = (int) $post->ID;
 
 		$single = array(
-			'id'        => $id ,
-			'title'     => get_the_title() ,
-            'permalink' => get_permalink(),
-            'content'   => get_the_content(),
-            'excerpt'   => get_the_excerpt(),
-			'date'      => get_the_date('Y-m-d H:i:s','','',false) ,
-			'author'    => get_the_author() ,
+			'contentUri' => get_bloginfo('url'), // not used
+			'title' => get_the_title(),
+			'body' => get_the_excerpt_max_charlength(16) . '...', // not used
+			'urls' => array(
+				'pcUrl' => get_permalink(),
+				'mobileUrl' => '', // not used
+				'smartphoneUrl' => '',// not used
+				),
+			'images' => array(
+				'force_array' => 1,
+				'large' => '',// not used
+				'medium' => '',// not used
+				'small' => 'http://alpha.mixi.co.jp/blog/wp-content/uploads/2011/12/40x40.png',
+				),
+			'user' => array(
+				'id' => '', // not used
+				'displayName' => '', // not used
+				'thumbnailUrl' => '', // not used
+				),
+			'favoriteCount' => '', // not used
+			'commentCount' => '', // not used
+			'sourceName' => '', // not used
+			'created' => get_the_date('c','','',false) ,
 			);
-
-		// thumbnail
-		if (function_exists('has_post_thumbnail') && has_post_thumbnail($id)) {
-			$single["thumbnail"] = preg_replace("/^.*['\"](https?:\/\/[^'\"]*)['\"].*/i","$1",get_the_post_thumbnail($id));
-		}
-
-		// category
-		$categories = array();
-		foreach((get_the_category()) as $category) { 
-			$categories[] = $category->cat_name; 
-		}
-		$single["categories"] = $categories;
-
-		// tag
-		$tags = array();
-		foreach((get_the_tags()) as $tag) { 
-			$tags[] = $tag->name; 
-		}
-		$single["tags"] = $tags;
-
 		$json[] = $single;
 	}
-	$json = json_encode($json);
+
+	$json = '{"entry" : ' . my_json_encode($json) . '}';
 
 	nocache_headers();
 	if (!empty($callback)) {
